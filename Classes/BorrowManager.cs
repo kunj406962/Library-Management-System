59a9911c4121f9d7c540e344ff2352f43edad3f8
Kunj Patel <pkunjk007@gmail.com>
2025-04-09 15:52:28 -0600

    Initial Commit Created all necessay class .cs files

---
+++ Classes/BorrowManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211FinalProject.Classes
+{
+    internal class BorrowManager
+    {
+    }
+}
